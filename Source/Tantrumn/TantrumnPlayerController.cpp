f8b8c460fea8f5561f8e27f068b77931d2d8e482
Archie <35892559+ArchieArch1@users.noreply.github.com>
2021-11-17 10:20:25 +0000

    Added basic character movement
    Basic character movement added

---
+++ Source/Tantrumn/TantrumnPlayerController.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TantrumnPlayerController.h"
+#include "GameFramework/Character.h"
+
+void ATantrumnPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	if(InputComponent)
+	{
+		InputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &ATantrumnPlayerController::OnJumpAction);
+		InputComponent->BindAxis("MoveForward", this, &ATantrumnPlayerController::RequestMoveForward);
+		InputComponent->BindAxis("MoveRight", this, &ATantrumnPlayerController::RequestMoveRight);
+		InputComponent->BindAxis("LookUp", this, &ATantrumnPlayerController::RequestLookUp);
+		InputComponent->BindAxis("LookRight", this, &ATantrumnPlayerController::RequestLookRight);
+	}
+}
+
+void ATantrumnPlayerController::OnJumpAction()
+{
+	if(GetCharacter())
+	{
+		GetCharacter()->Jump();
+	}
+}
+
+void ATantrumnPlayerController::RequestMoveForward(float AxisValue)
+{
+	if(AxisValue != 0.f)
+	{
+		FRotator const ControlSpaceRot = GetControlRotation();
+		//Transform to world space and add it
+		GetPawn()->AddMovementInput(FRotationMatrix(ControlSpaceRot).GetScaledAxis(EAxis::X), AxisValue);
+	}
+}
+
+void ATantrumnPlayerController::RequestMoveRight(float AxisValue)
+{
+	if(AxisValue != 0.f)
+	{
+		FRotator const ControlSpaceRot = GetControlRotation();
+		//Transform to world space and add it
+		GetPawn()->AddMovementInput(FRotationMatrix(ControlSpaceRot).GetScaledAxis(EAxis::Y), AxisValue);
+	}
+}
+
+void ATantrumnPlayerController::RequestLookUp(float AxisValue)
+{
+	AddPitchInput(AxisValue * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ATantrumnPlayerController::RequestLookRight(float AxisValue)
+{
+	AddYawInput(AxisValue * BaseLookRightRate * GetWorld()->GetDeltaSeconds());
+}
